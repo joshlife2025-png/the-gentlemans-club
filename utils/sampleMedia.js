1753e3aed534a44d1e13bdc839b1161d4adb93af
joshlife2025-png <joshlife2025@gmail.com>
2026-03-26 23:49:41 -0400

    Update sampleMedia.js

--- utils/sampleMedia.js
+++ utils/sampleMedia.js
@@ -1,4 +1,14 @@
 export default [
-  {id:'m1', title:'Preview', thumbnail:'https://picsum.photos/400/300?random=1', isPremium:false},
-  {id:'m2', title:'Premium Clip', thumbnail:'https://picsum.photos/400/300?random=2', isPremium:true}
+  {
+    id: 'm1',
+    title: 'Preview',
+    thumbnail: 'https://picsum.photos/400/300?random=1',
+    isPremium: false,
+  },
+  {
+    id: 'm2',
+    title: 'Premium Clip',
+    thumbnail: 'https://picsum.photos/400/300?random=2',
+    isPremium: true,
+  },
 ]
