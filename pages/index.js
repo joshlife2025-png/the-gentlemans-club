cdb2ade7283023e0f6b8ff1c76d62654f951a64b
joshlife2025-png <joshlife2025@gmail.com>
2026-03-26 22:27:25 -0400

    Replace header and main content with test page

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,10 @@
-import Header from '../components/Header'
-import Gallery from '../components/Gallery'
 export default function Home() {
   return (
-    <div className="relative z-10">
-      <Header />
-      <main className="max-w-6xl mx-auto p-6">
-        <h1 className="text-3xl font-bold">
-          Welcome to The Gentleman's Club
-        </h1>
-      </main>
+    <div className="p-6">
+      <h1>Test Page</h1>
+      <button onClick={() => alert("works")}>
+        Click Me
+      </button>
     </div>
   )
 }
