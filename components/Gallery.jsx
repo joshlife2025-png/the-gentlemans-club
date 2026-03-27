f74cfb1e7b1f88718f60e27a5b51eb0f7bd20166
joshlife2025-png <joshlife2025@gmail.com>
2026-03-26 23:31:13 -0400

    Update Gallery component to use MediaCard
    Refactor Gallery component to display media cards from sampleMedia.

--- components/Gallery.jsx
+++ components/Gallery.jsx
@@ -1,7 +1,12 @@
-export default function Gallery(){
+import sampleMedia from '../utils/sampleMedia'
+import MediaCard from './MediaCard'
+
+export default function Gallery({ isPremiumUser }) {
   return (
-    <div className="mt-6">
-      <p>Gallery loading...</p>
+    <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4 mt-6">
+      {sampleMedia.map((m) => (
+        <MediaCard key={m.id} item={m} isPremiumUser={isPremiumUser} />
+      ))}
     </div>
   )
 }
