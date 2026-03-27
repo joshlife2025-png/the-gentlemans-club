c138cb2ed5b354c784d52a68599ec9ccf9e4659f
joshlife2025-png <joshlife2025@gmail.com>
2026-03-26 23:20:13 -0400

    Add Gallery component and update layout

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
 import Header from '../components/Header'
+import Gallery from '../components/Gallery'
 
 export default function Home() {
   return (
-    <div className="p-6">
+    <div className="min-h-screen bg-gray-100 relative z-10">
       <Header />
-      <button onClick={() => alert("works")}>
-        Click Me
-      </button>
+      <main className="max-w-6xl mx-auto p-6">
+        <h1 className="text-3xl font-bold">Welcome to The Gentleman's Club</h1>
+        <Gallery /> {/* Make sure this is uncommented */}
+      </main>
     </div>
   )
 }
