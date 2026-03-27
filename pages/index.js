94d348996ce7ffa774105a267f7991cbf6ce82f1
joshlife2025-png <joshlife2025@gmail.com>
2026-03-26 22:06:43 -0400

    Refactor Home component layout and indentation

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,12 @@ import Header from '../components/Header'
 import Gallery from '../components/Gallery'
 export default function Home() {
   return (
-    <div>
+    <div className="relative z-10">
       <Header />
       <main className="max-w-6xl mx-auto p-6">
-        <h1 className="text-3xl font-bold">Welcome to The Gentleman's Club</h1>
-        <Gallery />
+        <h1 className="text-3xl font-bold">
+          Welcome to The Gentleman's Club
+        </h1>
       </main>
     </div>
   )
